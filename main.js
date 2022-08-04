41b4431185d6676197e77a909b11732d4d6e5161
marssolar <93085068+marssolar@users.noreply.github.com>
2022-08-04 09:59:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function preload()
+{
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function setup()
+{
+    
+    canvas = createCanvas(390,350);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function draw()
+{
+ strokeWeight(10)
+ stroke(0);
+ if(mouseIsPressed)
+ {
+    line(pmouseX,pmouseY,mouseX,mouseY)
+ }
+}
+
+function clearCanvas()
+{
+    background("white")
+}
+
+function classifyCanvas()
+{
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResult(error, results)
+{
+     if (error) {
+ console.error(error);
+     }
+     console.log(results)
+     document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+
+     document.getElementById('confidence').innerHTML = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+
+     utterThis = new SpeechSynthesisUtterance (results[0].label);
+     synth.speak(utterThis);
+}
